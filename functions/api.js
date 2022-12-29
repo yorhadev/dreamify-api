4e5c87da9398bd464b8f1b5ef116e7174ab8232d
yorhadev <rafael.takano.dev@gmail.com>
2022-12-29 06:56:07 -0300

    refactor(api): config open-ai

--- functions/api.js
+++ functions/api.js
@@ -1,5 +1,6 @@
 import * as dotenv from "dotenv";
 import express, { Router } from "express";
+import cors from "cors";
 import serverless from "serverless-http";
 import { Configuration, OpenAIApi } from "openai";
 
@@ -10,14 +11,21 @@ const router = Router();
 const configuration = new Configuration({ apiKey: process.env.OPENAI });
 const openai = new OpenAIApi(configuration);
 
+app.use(cors());
+app.use(express.json());
+app.use("/.netlify/functions/api", router);
+
 router.get("/", (req, res) => {
   res.json({
-    hello: "world",
+    hello: "worldz",
   });
 });
 
-router.get("v1/create-image");
-
-app.use("/.netlify/functions/api", router);
+router.get("v1/create", async (req, res) => {
+  res.json({
+    firstName: "lalalala",
+    lastName: "lolololo",
+  });
+});
 
 export const handler = serverless(app);
